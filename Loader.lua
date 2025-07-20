5ab59e8e94a73de9de008db538b215592d9bd4d0
HKTwoScript <jancmafs@gmail.com>
2025-07-20 11:09:08 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,7 @@
 pcall(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/f4shn/main/refs/heads/main/games/gagvisual.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/f4shn/main/refs/heads/main/games/gagvisual.lua"))("discord.gg/csxu2nCkw9")
 end)
 
 pcall(function()
-    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/f1It2DyEU8/0", true))()
+    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/f1It2DyEU8/0"))()
 end)
